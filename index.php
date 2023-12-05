de1b822d06689032de8d4e2970ed897d87b47efc
drrlldc <delacruzdarrelljohnd@gmail.com>
2023-12-05 19:09:14 +0800

    Change H1

--- index.php
+++ index.php
@@ -8,8 +8,7 @@
 </head>
 
 <body>
-
-    <h1>Third Version</h1>
+    <h1>Darrell's Version</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium a magni facere quasi mollitia, rerum totam praesentium laborum modi nulla quisquam? Minus incidunt hic libero tenetur labore voluptates provident minima possimus odit eos. Sed fugit eos ad libero optio illum maxime omnis, velit nostrum eum a similique voluptates veritatis nisi.</p>
 </body>
 
